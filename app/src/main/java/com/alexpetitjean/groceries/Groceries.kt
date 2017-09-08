22a41377ce61f09eeab7ca9630d6ce1159d67d0c
Alex Petitjean <alex@tenforwardconsulting.com>
2017-09-08 18:36:14 -0500

    playing around with MaterialDrawer library

--- app/src/main/java/com/alexpetitjean/groceries/Groceries.kt
+++ app/src/main/java/com/alexpetitjean/groceries/Groceries.kt
@@ -2,11 +2,23 @@ package com.alexpetitjean.groceries
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.joanzapata.iconify.Iconify
+import com.joanzapata.iconify.fonts.MaterialModule
+import com.squareup.leakcanary.LeakCanary
 
 class Groceries : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // This must be the first thing in onCreate
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.enableDisplayLeakActivity(this)
+        LeakCanary.install(this)
+
         Stetho.initializeWithDefaults(this)
+
+        Iconify.with(MaterialModule())
     }
 }
--- app/src/main/java/com/alexpetitjean/groceries/ui/ShoppingListsActivity.kt
+++ app/src/main/java/com/alexpetitjean/groceries/ui/ShoppingListsActivity.kt
@@ -1,14 +1,26 @@
 package com.alexpetitjean.groceries.ui
 
+import android.annotation.SuppressLint
 import android.arch.lifecycle.LifecycleActivity
 import android.arch.persistence.room.Room
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.SystemClock
 import android.support.v7.widget.LinearLayoutManager
+import co.zsmb.materialdrawerkt.builders.accountHeader
+import co.zsmb.materialdrawerkt.builders.drawer
+import co.zsmb.materialdrawerkt.builders.footer
+import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
+import co.zsmb.materialdrawerkt.draweritems.badgeable.secondaryItem
+import co.zsmb.materialdrawerkt.draweritems.divider
+import co.zsmb.materialdrawerkt.draweritems.profile.profile
 import com.alexpetitjean.groceries.data.GroceriesDatabase
 import com.alexpetitjean.groceries.data.ShoppingList
 import com.alexpetitjean.groceries.toast
 import com.alexpetitjean.myapplication.R
 import com.jakewharton.rxbinding2.view.RxView
+import com.joanzapata.iconify.IconDrawable
+import com.joanzapata.iconify.fonts.MaterialIcons
 import com.trello.rxlifecycle2.android.lifecycle.kotlin.bindToLifecycle
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.subscribeBy
@@ -25,6 +37,48 @@ class ShoppingListsActivity : LifecycleActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_lists)
 
+        drawer {
+            accountHeader {
+                background = R.color.accent
+                onlyMainProfileImageVisible = true
+
+                profile("George", "george@george.com") {
+                    iconDrawable = IconDrawable(this@ShoppingListsActivity, MaterialIcons.md_person)
+                }
+                profile("Alabaster", "alabaster@dumbnames.com") {
+                    iconDrawable = IconDrawable(this@ShoppingListsActivity, MaterialIcons.md_person_outline)
+                }
+                profile("Winchester", "winchester@dumbnames.com") {
+                    iconDrawable = IconDrawable(this@ShoppingListsActivity, MaterialIcons.md_person_pin)
+                }
+
+                onProfileChanged { _, profile, current ->
+                    if (current) {
+                        toast("No switch")
+                    } else {
+                        toast("Switching to ${profile.name}")
+                    }
+                    true
+                }
+            }
+            headerViewRes = R.layout.material_drawer_header
+            primaryItem("Lists") {}
+            primaryItem("Items") {}
+            divider {}
+            footer {
+                secondaryItem("Settings") {}
+            }
+
+            onItemClick { view, position, drawerItem ->
+                toast("$position $drawerItem")
+                true
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
         val shoppingListDao = db.shoppingListDao()
 
         RxView.clicks(addShoppingListButton)
@@ -37,6 +91,8 @@ class ShoppingListsActivity : LifecycleActivity() {
                             if (name.isNotEmpty()) {
                                 val newList = ShoppingList(name = name)
                                 shoppingListDao.insertAll(newList)
+                            } else {
+                                startLeak()
                             }
                         })
 
@@ -58,8 +114,18 @@ class ShoppingListsActivity : LifecycleActivity() {
                 .subscribeBy(onNext = { shoppingListsAdapter.setShoppingLists(it) })
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onPause() {
+        super.onPause()
         db.close()
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private fun startLeak() {
+        object : AsyncTask<Void, Void, Void>() {
+            override fun doInBackground(vararg voids: Void): Void? {
+                SystemClock.sleep(20000)
+                return null
+            }
+        }.execute()
     }
 }
